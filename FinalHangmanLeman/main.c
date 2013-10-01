6a544cbbe1ed00d980f213952df23366b279aa44
Zleman <zac@dhcp-208-50.wireless.bowdoin.edu>
2013-10-01 11:27:50 -0400

    Initial Commit

---
+++ FinalHangmanLeman/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  FinalHangmanLeman
+//
+//  Created by Zleman on 10/1/13.
+//  Copyright (c) 2013 Zleman. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
